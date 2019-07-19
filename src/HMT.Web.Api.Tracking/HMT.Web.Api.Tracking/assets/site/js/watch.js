5487e9725c6632b3abd439b449d1a8281295d7a2
Azmi ŞAHİN <azmisahin@outlook.com>
2019-07-19 13:59:30 +0300

    Get Service Job Events with vue and jquery

---
+++ src/HMT.Web.Api.Tracking/HMT.Web.Api.Tracking/assets/site/js/watch.js
@@ -0,0 +1,60 @@
+﻿console.log("watch start");
+
+// Change Service Domain Eg: http://localhost:85 or http://api.domain.com
+var serviceDomain = location.origin;
+
+// Change Service Address
+var serviceApi = "/api/PrinterJob/Tracking";
+
+// Full Address
+var requestUri = serviceDomain + serviceApi;
+
+/**
+ * Perform an asynchronous HTTP (Ajax) request.
+ * ref: https://api.jquery.com/jquery.ajax/
+ * @param {string} uri string remote address
+ */
+function HttpRequest(uri) {
+
+    // Request Method
+    var method = "get";
+
+    // Setup
+    var resquest = $.ajax({
+        type: method,
+        url: uri
+    });
+
+    resquest
+        // Data Done
+        .done(function (data) {
+            // Write Data
+            BindData(data);
+        })
+        // Fail
+        .fail(function (jqXHR) {
+
+            // What is fail
+            console.log(jqXHR.statusText);
+        });
+}
+
+// Document Ready And Run
+$(document).ready(HttpRequest(requestUri));
+
+/**
+ * Bind Data
+ * @param {List<PrinterJob>} data Printer Job List
+ */
+function BindData(data) {
+
+    // vue bind
+    // ref: https://vuejs.org/v2/guide/
+
+    var app = new Vue({
+        el: '#app',
+        data: {
+            PrinterJobs: data
+        }
+    });
+}
